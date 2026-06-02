5bc5b10ceaee5e8f7540ffcb960b4c67a8ca55d1
jayme-github <jayme-github@users.noreply.github.com>
2026-06-02 15:29:41 +0200

    Bump version to 1.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyflop",
-    version="1.1.1",
+    version="1.1.2",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
